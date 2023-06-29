ddb193c24375b6f2dd6abdbc21f7a2809217a277
Diegoo11 <ynoacamino@gmail.com>
2023-06-29 00:12:39 -0500

    config init for apolloClient

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  ApolloClient, InMemoryCache, HttpLink, ApolloProvider,
+} from '@apollo/client';
 import App from './App';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:1111/',
+  }),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 );
