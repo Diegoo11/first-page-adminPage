ed9152d2cc69e37d5ffb780376c4856faf7f34a2
Diegoo11 <ynoacamino@gmail.com>
2023-07-06 21:08:34 -0500

    conect error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
 import LoginForm from './LoginForm';
 import Home from './Home/Home';
+import Loading from './Loading/Loading';
+import ScreenError from './ScreenError/ScreenError';
 
 function App() {
   const [token, setToken] = useState(null);
@@ -9,6 +12,17 @@ function App() {
       setToken(localStorage.getItem('admin-login-token'));
     }
   }, []);
+
+  const { loading, data, error } = useQuery(gql`
+  query{
+    testConnection
+  }
+`);
+  // eslint-disable-next-line no-console
+  console.log(data ? data.testConnection : 'Loading');
+  if (error) return <ScreenError errorCode={error.code} msgError={error.message} />;
+  if (loading) return <Loading />;
+
   return (
     <div>
       {!token && <LoginForm setToken={setToken} />}
