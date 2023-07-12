962376b1640934cb0cac2e7f3d5d3e7635c851d4
Diegoo11 <ynoacamino@gmail.com>
2023-07-11 19:08:48 -0500

    accordion InicioIndex added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { cyan } from '@mui/material/colors';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import HeaderIndex from './Header/HeaderIndex';
 import InicioIndex from './Inicio/InicioIndex';
 
@@ -16,9 +17,17 @@ const defaultTheme = createTheme({
 function Home() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="lg">
-
-        <InicioIndex />
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{
+          padding: 10,
+        }}
+      >
+        <Stack spacing={3}>
+          <HeaderIndex />
+          <InicioIndex />
+        </Stack>
       </Container>
     </ThemeProvider>
   );
