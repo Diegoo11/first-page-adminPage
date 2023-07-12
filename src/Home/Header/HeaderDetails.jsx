b93905fccea8dafb46619ca369d0edea07c71ad4
Diegoo11 <ynoacamino@gmail.com>
2023-07-11 19:10:39 -0500

    components abstracted and handleError added

--- src/Home/Header/HeaderDetails.jsx
+++ src/Home/Header/HeaderDetails.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-syntax */
+import React from 'react';
 import { useMutation } from '@apollo/client';
-import { findID, FormImage, FormText } from '../../util';
+import {
+  findID, FormImage, FormText, FormBox, useForm,
+} from '../../util';
 import { UPDATE_MODULE } from './Mutation';
 
 function HeaderDetails({ images, texts }) {
-  const idImageLogo = '649e0f0de59b27af2756c89d';
-  const imgLogo = images.find(findID(idImageLogo));
-  const idTextLogo = '649f8b8fe527c66bfbf3f850';
-  const textLogo = texts.find(findID(idTextLogo));
+  const imgLogo = images.find(findID('649e0f0de59b27af2756c89d'));
+  const textLogo = texts.find(findID('649f8b8fe527c66bfbf3f850'));
 
+  const [refImgLogo, gdImageLogo] = useForm();
+  const [refTextLogo, gdTextLogo] = useForm();
+  /*
   const [src, setSrc] = useState(imgLogo.src);
   const [alt, setAlt] = useState(imgLogo.alt);
   const [srcMobile, setSrcMobile] = useState(imgLogo.srcMobile);
@@ -21,8 +22,6 @@ function HeaderDetails({ images, texts }) {
   const [name, setName] = useState(textLogo.name);
   const [description, setDescription] = useState(textLogo.description);
   const [textLink, setTextLink] = useState(textLogo.link);
-
-  const [changes, setChanges] = useState(true);
 
   useEffect(() => {
     if (src !== imgLogo.src
@@ -38,13 +37,22 @@ function HeaderDetails({ images, texts }) {
       setChanges(true);
     }
   }, [src, alt, srcMobile, imgLink, name, description, textLink]);
-
-  const [updateModule, { loading, error }] = useMutation(UPDATE_MODULE);
-  if (loading) return 'Submitting...';
-  if (error) { console.log(error); return `Submission error! ${error.message}`; }
+*/
+  const [editModule, { loading, error }] = useMutation(UPDATE_MODULE);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log({
+      ImgLogo: gdImageLogo(),
+      TextLogo: gdTextLogo(),
+    });
+    editModule({
+      variables: {
+        images: [gdImageLogo()],
+        texts: [gdTextLogo()],
+      },
+    });
+    /*
     updateModule({
       variables: {
         editImageId: idImageLogo,
@@ -57,63 +65,23 @@ function HeaderDetails({ images, texts }) {
         description,
         textLink,
       },
-    });
+    }); */
   };
 
+  if (loading) return 'Submitting...';
+  if (error) { console.log(error); return `Submission error! ${error.message}`; }
+
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      noValidate
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        paddingTop: 3,
-        paddingBottom: 3,
-      }}
-    >
-      <Grid
-        container
-        maxWidth="md"
-        sx={{
-          marginLeft: -3,
-          width: '100%',
-          display: 'flex',
-        }}
-        rowSpacing={2}
-      >
-        <FormImage
-          img={imgLogo}
-          src={src}
-          alt={alt}
-          srcMobile={srcMobile}
-          imgLink={imgLink}
-          setSrc={setSrc}
-          setAlt={setAlt}
-          setSrcMobile={setSrcMobile}
-          setImgLink={setImgLink}
-        />
-        <FormText
-          text={textLogo}
-          name={name}
-          description={description}
-          textLink={textLink}
-          setName={setName}
-          setDescription={setDescription}
-          setTextLink={setTextLink}
-        />
-        <Grid item xs={10} />
-        <Grid item xs={2}>
-          <Button type="submit" fullWidth variant="contained" disabled={changes}>
-            <Typography component="h1" variant="h6" color="white">
-              Save
-            </Typography>
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+    <FormBox handleSubmit={handleSubmit} changes={false}>
+      <FormImage
+        ref={refImgLogo}
+        img={imgLogo}
+      />
+      <FormText
+        ref={refTextLogo}
+        text={textLogo}
+      />
+    </FormBox>
   );
 }
 
