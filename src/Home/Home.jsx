be4bbe14ced9619dad7c6d67a7b8613e8315585a
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2023-07-15 20:41:00 -0500

    sidebar added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,34 +1,82 @@
 import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { cyan } from '@mui/material/colors';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
+import {
+  Box, List, ListItem, Divider, ListItemButton, ListItemIcon, Drawer, ListItemText, Toolbar,
+} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import HeaderIndex from './Header/HeaderIndex';
 import InicioIndex from './Inicio/InicioIndex';
 
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: cyan[500],
+      main: '#000000',
     },
   },
 });
 
+const drawerWidth = 300;
+
 function Home() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container
-        component="main"
-        maxWidth="lg"
-        sx={{
-          padding: 10,
-        }}
-      >
-        <Stack spacing={3}>
-          <HeaderIndex />
-          <InicioIndex />
-        </Stack>
-      </Container>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Toolbar />
+          <Divider />
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Container
+          component="main"
+          maxWidth="xl"
+          sx={{
+            padding: 10,
+          }}
+        >
+          <Stack spacing={3}>
+            <HeaderIndex />
+            <InicioIndex />
+          </Stack>
+        </Container>
+      </Box>
     </ThemeProvider>
   );
 }
