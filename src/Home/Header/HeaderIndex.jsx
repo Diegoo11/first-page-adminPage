51db0d61633ebb840e8aa8a7fe319f761c29de4a
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 20:53:09 -0500

    remove innesesary accordion

--- src/Home/Header/HeaderIndex.jsx
+++ src/Home/Header/HeaderIndex.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import {
-  Accordion, AccordionSummary, CircularProgress, Typography,
+  Accordion, AccordionSummary, Box, CircularProgress, Typography,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ViewInArIcon from '@mui/icons-material/ViewInAr';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import { useQuery } from '@apollo/client';
-import { grey } from '@mui/material/colors';
-import ViewInArIcon from '@mui/icons-material/ViewInAr';
 import HeatherSummary from './HeaderSummary';
 import HeatherDetails from './HeaderDetails';
 import { QUERY_MODULE } from '../../Operations/query';
@@ -17,58 +16,45 @@ function HeaderIndex() {
       id: '64a4ced84c622ac3d7588672',
     },
   });
-
-  const [acor1, setAcor1] = useState(false);
-
-  const handleChange = () => {
-    setAcor1((n) => !n);
-  };
   const [subAcor1, setSubAcor1] = useState(false);
 
   const suBHangleChange = () => {
     setSubAcor1((n) => !n);
   };
   return (
-    <Accordion expanded={acor1} onChange={handleChange} sx={{ bgcolor: grey[100] }}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        id="header"
-      >
+    <>
+      <Box display="flex" alignItems="center">
         <ViewInArIcon sx={{ m: 2 }} />
-        <Typography sx={{ display: 'flex', alignItems: 'center' }} component="h1" variant="h5">
-          Header
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Accordion
-          expanded={subAcor1}
-          onChange={suBHangleChange}
+        <Typography variant="h5" component="h1">Header</Typography>
+      </Box>
+      <Accordion
+        expanded={subAcor1}
+        onChange={suBHangleChange}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
         >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-          >
-            {loading
-              ? <CircularProgress />
-              : (
-                <HeatherSummary
-                  images={data.findForModule.image}
-                  texts={data.findForModule.text}
-                />
-              )}
-          </AccordionSummary>
-          <AccordionDetails>
-            {loading
-              ? <CircularProgress />
-              : (
-                <HeatherDetails
-                  images={data.findForModule.image}
-                  texts={data.findForModule.text}
-                />
-              )}
-          </AccordionDetails>
-        </Accordion>
-      </AccordionDetails>
-    </Accordion>
+          {loading
+            ? <CircularProgress />
+            : (
+              <HeatherSummary
+                images={data.findForModule.image}
+                texts={data.findForModule.text}
+              />
+            )}
+        </AccordionSummary>
+        <AccordionDetails>
+          {loading
+            ? <CircularProgress />
+            : (
+              <HeatherDetails
+                images={data.findForModule.image}
+                texts={data.findForModule.text}
+              />
+            )}
+        </AccordionDetails>
+      </Accordion>
+    </>
   );
 }
 
--- src/Home/Inicio/InicioIndex.jsx
+++ src/Home/Inicio/InicioIndex.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  Accordion, CircularProgress, AccordionDetails, AccordionSummary, Typography, Stack,
+  CircularProgress, Box, Typography, Stack,
 } from '@mui/material';
 import { useQuery } from '@apollo/client';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ViewInArIcon from '@mui/icons-material/ViewInAr';
-import { grey } from '@mui/material/colors';
 import Background from './Background/Background';
 import { QUERY_MODULE } from '../../Operations/query';
 import Slider from './Slider/Slider';
 import Products from './Products/Products';
 
 function InicioIndex() {
-  const [open, setOpen] = useState(false);
   const { loading, data } = useQuery(QUERY_MODULE, {
     variables: {
       id: '64a4dbacb464c4051e9b4a73',
@@ -29,29 +26,21 @@ function InicioIndex() {
     );
   }
 
-  const hangleChange = () => {
-    setOpen((o) => !o);
-  };
-
   const images = data.findForModule.image;
   const texts = data.findForModule.text;
 
   return (
-    <Accordion expanded={open} onChange={hangleChange} sx={{ bgcolor: grey[100] }}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+    <>
+      <Box display="flex" alignItems="center">
         <ViewInArIcon sx={{ m: 2 }} />
-        <Typography sx={{ display: 'flex', alignItems: 'center' }} component="h1" variant="h5">
-          Inicio
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Stack spacing={3}>
-          <Background images={images} texts={texts} />
-          <Slider images={images} texts={texts} />
-          <Products images={images} texts={texts} />
-        </Stack>
-      </AccordionDetails>
-    </Accordion>
+        <Typography variant="h5" component="h1">Inicio</Typography>
+      </Box>
+      <Stack spacing={3}>
+        <Background images={images} texts={texts} />
+        <Slider images={images} texts={texts} />
+        <Products images={images} texts={texts} />
+      </Stack>
+    </>
   );
 }
 
