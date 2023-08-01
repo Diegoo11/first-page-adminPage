a515954e825d7ebcdc93d08875f079a66c8b88a8
Diegoo11 <ynoacamino@gmail.com>
2023-07-31 21:19:54 -0500

    fix connection error

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  ApolloClient, InMemoryCache, HttpLink, ApolloProvider,
+  ApolloClient, InMemoryCache, HttpLink, ApolloProvider, from,
 } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import App from './App';
 import { AdminProvider } from './context/AdminContext';
 
 const getAuth = () => {
   const tk = localStorage.getItem('admin-login-token');
-  return tk ? `admin ${tk}` : null;
+  return tk ? `admin ${tk}` : '';
 };
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => console.log(
+      `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+    ));
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://first-page-backend-production.up.railway.app/',
+  headers: {
+    authorization: getAuth(),
+  },
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    headers: {
-      authorization: getAuth(),
-    },
-    uri: 'https://first-page-backend-production.up.railway.app/',
-  }),
+  link: from([errorLink, httpLink]),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
