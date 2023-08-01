9927c61fb44cb2a302dcc6ff23c01ac8c1f611e4
Diegoo11 <ynoacamino@gmail.com>
2023-07-31 21:00:26 -0500

    add querys and mutations

--- src/Operations/query.js
+++ src/Operations/query.js
@@ -30,3 +30,52 @@ export const QUERY_MODULE = gql`
     }
   }
 `;
+
+export const QUERY_ONE_ITEM = gql`
+query($id: String!) {
+  findItemById(id: $id) {
+    name
+    src
+    id
+    cost
+    brand
+    description
+    stock
+    tags
+  }
+}
+`;
+
+export const QUERY_BRANDS = gql`
+query{
+  findBrands {
+    id
+    name
+    src
+  }
+}
+`;
+
+export const QUERY_SHOPS = gql`
+{
+  findShops {
+    name
+    city
+    src
+    direcction
+    email
+    phone
+    workingHours
+    id
+  }
+}
+`;
+
+export const QUERY_ADMINDATA = gql`
+{
+  findAdminData {
+    username
+    id
+  }
+}
+`;
